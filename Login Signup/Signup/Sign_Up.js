f6d72290b7783ded1327f5b316da21e68b75e925
unknown <mukundshil1234@gmail.com>
2021-04-08 23:01:37 +0530

    patient file updated

--- Login Signup/Signup/Sign_Up.js
+++ Login Signup/Signup/Sign_Up.js
@@ -1,14 +1,12 @@
 function Signup() {
-    alert("welcome!!!");
+    // alert("welcome!!!");
 
     const Form = document.getElementById("SignUp");
     const fname = Form.fname.value;
     const mname = Form.mname.value;
     const lname = Form.lname.value;
     const dob = Form.dob.value;
-    // const male = Form.male.value;
-    // const female = Form.female.value;
-    // const other = Form.other.value;
+
     const Mobile = Form.Mobile.value;
     const email = Form.email.value;
     const pwd = Form.pwd.value;
@@ -16,20 +14,18 @@ function Signup() {
 
 
 
-    if (pwd.value === cpwd.value) {
+    if (pwd === cpwd) {
         alert("Registration Sucessful");
         localStorage.setItem('name', fname);
         localStorage.setItem('mname', mname);
         localStorage.setItem('lname', lname);
         localStorage.setItem('dob', dob);
-        // localStorage.setItem('male', document.getElementById("male").checked);
-        // localStorage.setItem('female', document.getElementById("female").checked);
-        // localStorage.setItem('other', document.getElementById("other").checked);
+
         localStorage.setItem('Mobile', Mobile);
         localStorage.setItem('email', email);
         localStorage.setItem('pwd', pwd);
         window.open("http://127.0.0.1:5501/Login%20Signup/Login/Login.html");
-        // window.open("D:\FrontEnd\hosto mart\Login Signup\Login\Login.html");
+
     } else {
         alert("User Id or Passward is Invalid");
     }
